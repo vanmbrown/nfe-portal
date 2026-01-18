5e363056c4b9cbc1e7b05de25156898db3e1aaff
vanmbrown <vanessa.mccaleb@gmail.com>
2026-01-17 21:59:30 -0500

    Simplify OpenNext config and wrangler settings

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,6 +1,1 @@
-import { defineConfig } from "@opennextjs/cloudflare";
-import type { OpenNextConfig } from "@opennextjs/aws";
-
-const config: OpenNextConfig = {};
-
-export default defineConfig(config);
+export default {};
