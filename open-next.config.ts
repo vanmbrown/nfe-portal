93a40e2423aa09e17e4c69b1f8c726e8336b45dc
vanmbrown <vanessa.mccaleb@gmail.com>
2026-01-17 22:39:43 -0500

    Use defineCloudflareConfig in OpenNext config

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,1 +1,4 @@
-export default {};
+import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
+
+// Minimal config to satisfy OpenNext in CI (no prompts).
+export default defineCloudflareConfig({});
