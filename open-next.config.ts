84c3a47471fbb001c52764cd493a9a68673b15c0
vanmbrown <vanessa.mccaleb@gmail.com>
2026-01-17 21:44:09 -0500

    Add OpenNext config and Wrangler settings for CI builds

---
+++ open-next.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from "@opennextjs/cloudflare";
+import type { OpenNextConfig } from "@opennextjs/aws";
+
+const config: OpenNextConfig = {};
+
+export default defineConfig(config);
